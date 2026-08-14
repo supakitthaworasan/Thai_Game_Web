b77ef6754cd8f7eecfdc520b63d09bd0c431e365
supakitthaworasan <supakitthaworasan@gmail.com>
2026-08-14 15:01:50 +0700

    Add more error handlering

--- src/config/db.js
+++ src/config/db.js
@@ -9,4 +9,22 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 });
 
-export default pool;
+
+const connectDB = async ()=>{
+
+    try{
+        const client = await pool.connect();
+        console.log('DATABASE CONNECTED VIA PostgreSQL');
+        client.release();
+    }catch(err){
+        console.error('DATABASE CONNECTION FAILED:', err.message);
+        process.exit(1);
+
+    }
+}
+
+const disconnectDB = async ()=>{
+    await pool.end();
+}
+
+export {pool, connectDB, disconnectDB};
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import movieRoute from './routes/movieRoute.js';
-import pool from './config/db.js';
+import {pool, connectDB, disconnectDB} from './config/db.js';
 
 const app = express();
 
@@ -9,14 +9,42 @@ const PORT = 5001;
 app.use(express.json());
 app.use("/movies", movieRoute);
 
-app.listen(PORT, async ()=>{
+const startServer = async ()=>{
+    await connectDB();
+
+    app.listen(PORT, async ()=>{
     console.log(`Server is running on port ${PORT}`);
+        });
+};
 
-    try{
-        const result = await pool.query('SELECT NOW()');
-        console.log('DATABASE CONNECTION SUCCESSFUL:', result.rows[0]);
-    }catch(err){
-        console.error('DATABASE CONNECTON FAILED:', err.message);
-    }
+startServer();
 
+process.on("unhandledRejection",(err)=>{
+    console.error("UNHANDLED error:", err);
+    server.close(async ()=>{
+        await disconnectDB();
+        process.exit(1);
+    });
+});
+
+process.on("uncaughtException", async (err)=>{
+    console.error("UNCAUGHT Exception:", err);
+    await disconnectDB();
+    process.exit(1);
+});
+
+process.on("SIGTERM", async ()=>{
+    console.log("SIGTERM RECEIVED. Shutting down gracefully");
+    server.close(async ()=>{
+        await disconnectDB();
+        process.exit(0);
+    });
+});
+
+process.on("SIGINT", async ()=>{
+    console.log("SIGINT RECEIVED. Shutting down gracefully");
+    server.close(async ()=>{
+        await disconnectDB();
+        process.exit(0);
+    });
 });
